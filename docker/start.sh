4649c0374bf8549ef5b64f171402bad4a4c5d832
ad3n <surya.kejawen@gmail.com>
2019-03-13 11:11:52 +0700

    improvement

--- docker/start.sh
+++ docker/start.sh
@@ -7,6 +7,48 @@ do
     sed -i "s|\${${name}}|${value}|g" /etc/nginx/conf.d/default.conf
 done
 
+for name in APP_ENV
+do
+    eval value=\$$name
+    sed -i "s|\${${name}}|${value}|g" /etc/nginx/conf.d/default.conf
+done
+
+for name in APP_SECRET
+do
+    eval value=\$$name
+    sed -i "s|\${${name}}|${value}|g" /etc/nginx/conf.d/default.conf
+done
+
+for name in DATABASE_DRIVER
+do
+    eval value=\$$name
+    sed -i "s|\${${name}}|${value}|g" /etc/nginx/conf.d/default.conf
+done
+
+for name in DATABASE_SERVER_VERSION
+do
+    eval value=\$$name
+    sed -i "s|\${${name}}|${value}|g" /etc/nginx/conf.d/default.conf
+done
+
+for name in DATABASE_CHARSET
+do
+    eval value=\$$name
+    sed -i "s|\${${name}}|${value}|g" /etc/nginx/conf.d/default.conf
+done
+
+for name in DATABASE_URL
+do
+    eval value=\$$name
+    sed -i "s|\${${name}}|${value}|g" /etc/nginx/conf.d/default.conf
+done
+
+for name in REDIS_URL
+do
+    eval value=\$$name
+    sed -i "s|\${${name}}|${value}|g" /etc/nginx/conf.d/default.conf
+done
+
 if [ "$APP_ENV" = 'prod' ]; then
 	composer install --prefer-dist --no-dev --no-progress --no-suggest --optimize-autoloader --classmap-authoritative
 else
