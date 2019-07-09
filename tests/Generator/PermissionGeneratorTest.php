0f8a23510dd99ac875fc4b85e7b7882279572dc9
Aden Kejawen <surya.kejawen@gmail.com>
2019-07-09 22:33:18 +0700

    update dependencies and add more test

--- tests/Generator/PermissionGeneratorTest.php
+++ tests/Generator/PermissionGeneratorTest.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace KejawenLab\Semart\Skeleton\Tests\Generator;
 
+use KejawenLab\Semart\Skeleton\Generator\PermissionGenerator;
+use KejawenLab\Semart\Skeleton\Menu\MenuService;
+use KejawenLab\Semart\Skeleton\Security\Service\GroupService;
+use KejawenLab\Semart\Skeleton\Security\Service\RoleService;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 /**
@@ -18,7 +22,13 @@ class PermissionGeneratorTest extends KernelTestCase
 
     public function testGenerate()
     {
-        // @todo tests \KejawenLab\Semart\Skeleton\Generator\PermissionGenerator::generate()
-        $this->assertTrue(true);
+        $groupServiceMock = $this->getMockBuilder(GroupService::class)->disableOriginalConstructor()->getMock();
+        $menuServiceMock = $this->getMockBuilder(MenuService::class)->disableOriginalConstructor()->getMock();
+        $roleServiceMock = $this->getMockBuilder(RoleService::class)->disableOriginalConstructor()->getMock();
+
+        $reflection = new \ReflectionClass(Stub::class);
+
+        $generator = new PermissionGenerator($groupServiceMock, $menuServiceMock, $roleServiceMock);
+        $this->assertNull($generator->generate($reflection));
     }
 }
