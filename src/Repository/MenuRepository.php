72e192a936ae84f2082110eb04946e3147086f80
Aden Kejawen <surya.kejawen@gmail.com>
2019-07-10 15:07:41 +0700

    fix php doc

--- src/Repository/MenuRepository.php
+++ src/Repository/MenuRepository.php
@@ -21,7 +21,7 @@ class MenuRepository extends Repository
      * @param array      $criteria
      * @param array|null $orderBy
      *
-     * @return Menu|null
+     * @return object|null
      */
     public function findOneBy(array $criteria, array $orderBy = null): ?object
     {
