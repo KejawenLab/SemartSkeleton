963f6acee79e2c204932d8a1417f85b6fb298537
Abdul Malik Ikhsan <samsonasik@gmail.com>
2019-07-12 04:36:17 +0700

    using UuidBinaryType for UUID in primary key

--- src/Contract/Entity/PrimaryableTrait.php
+++ src/Contract/Entity/PrimaryableTrait.php
@@ -14,7 +14,7 @@ trait PrimaryableTrait
 {
     /**
      * @ORM\Id()
-     * @ORM\Column(type="uuid", unique=true)
+     * @ORM\Column(type="uuid_binary", unique=true)
      * @ORM\GeneratedValue(strategy="CUSTOM")
      * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
      *
