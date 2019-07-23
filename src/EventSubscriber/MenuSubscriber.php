1458eaf4bf77b235cc2ac046c9cfd9cbf10889eb
Aden Kejawen <surya.kejawen@gmail.com>
2019-07-23 15:38:57 +0700

    improve menu

---
+++ src/EventSubscriber/MenuSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KejawenLab\Semart\Skeleton\EventSubscriber;
+
+use KejawenLab\Semart\Skeleton\Application;
+use KejawenLab\Semart\Skeleton\Entity\Menu;
+use KejawenLab\Semart\Skeleton\Pagination\PaginationEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * @author Muhamad Surya Iksanudin <surya.iksanudin@gmail.com>
+ */
+class MenuSubscriber implements EventSubscriberInterface
+{
+    public function filterPagination(PaginationEvent $event) {
+        if ($event->getEntityClass() !== Menu::class) {
+            return;
+        }
+
+        $filter = $event->getRequest()->query->get('f');
+        if ($filter) {
+            $queryBuilder = $event->getQueryBuilder();
+            $queryBuilder->join(sprintf('%s.parent', $event->getJoinAlias('root')), 'p');
+            $queryBuilder->andWhere($queryBuilder->expr()->eq('p.id', $queryBuilder->expr()->literal($filter)));
+            $event->addJoinAlias('parent', 'p');
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            Application::PAGINATION_EVENT => [['filterPagination']],
+        ];
+    }
+}
